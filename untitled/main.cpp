2f9f6ddf7501d48aa86d0a466f683cb6a08e0eb3
Loki239 <atovmian-a@yandex.ru>
2022-09-19 19:11:12 +0300

    test_CLION

---
+++ untitled/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void settings_default();
+
+using namespace std;
+int main() {
+    settings_default();
+    return 0;
+}
+void settings_default()
+{
+    cout << "Default settings:" << "\n" << "g = 9.81" << "\n" << "pi = 3.14";
+}
